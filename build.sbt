3ce37c00607c786c18122a6c48c3b33eb41958c6
Timothy McCarthy <ebh042@gmail.com>
2020-11-03 14:08:53 +1100

    Staging environment for files in S3

--- build.sbt
+++ build.sbt
@@ -19,17 +19,21 @@ lazy val core = project
   .in(file("core"))
   .settings(settingsHelper.settingsForSubprojectCalled("core"))
   .settings(
-    libraryDependencies += "org.typelevel"                   %% "cats-effect"                % "2.2.0",
-    libraryDependencies += "au.id.tmm.ausgeo"                %% "ausgeo-core"                % "0.2.4",
-    libraryDependencies += "au.id.tmm.tmm-scala-collections" %% "tmm-scala-collections-core" % tmmCollectionsVersion,
-    libraryDependencies += "au.id.tmm.tmm-scala-collections" %% "tmm-scala-collections-cats" % tmmCollectionsVersion,
-    libraryDependencies += "au.id.tmm.tmm-utils"             %% "tmm-utils-syntax"           % tmmUtilsVersion,
-    libraryDependencies += "au.id.tmm.tmm-utils"             %% "tmm-utils-errors"           % tmmUtilsVersion,
-    libraryDependencies += "au.id.tmm.tmm-utils"             %% "tmm-utils-cats"             % tmmUtilsVersion,
-    libraryDependencies += "org.jsoup"                        % "jsoup"                      % "1.13.1",
-    libraryDependencies += "com.amazonaws"                    % "aws-java-sdk-textract"      % "1.11.890",
-    libraryDependencies += "org.slf4j"                        % "slf4j-api"                  % "1.7.30",
-    libraryDependencies += "org.slf4j"                        % "slf4j-simple"               % "1.7.30" % Runtime,
+    libraryDependencies += "org.typelevel"                   %% "cats-effect"                    % "2.2.0",
+    libraryDependencies += "au.id.tmm.ausgeo"                %% "ausgeo-core"                    % "0.2.4",
+    libraryDependencies += "au.id.tmm.tmm-scala-collections" %% "tmm-scala-collections-core"     % tmmCollectionsVersion,
+    libraryDependencies += "au.id.tmm.tmm-scala-collections" %% "tmm-scala-collections-cats"     % tmmCollectionsVersion,
+    libraryDependencies += "au.id.tmm.tmm-utils"             %% "tmm-utils-syntax"               % tmmUtilsVersion,
+    libraryDependencies += "au.id.tmm.tmm-utils"             %% "tmm-utils-errors"               % tmmUtilsVersion,
+    libraryDependencies += "au.id.tmm.tmm-utils"             %% "tmm-utils-cats"                 % tmmUtilsVersion,
+    libraryDependencies += "au.id.tmm.tmm-utils"             %% "tmm-utils-codec"                % tmmUtilsVersion,
+    libraryDependencies += "com.softwaremill.sttp.client3"   %% "core"                           % "3.0.0-RC7",
+    libraryDependencies += "com.softwaremill.sttp.client3"   %% "async-http-client-backend-cats" % "3.0.0-RC7",
+    libraryDependencies += "org.jsoup"                        % "jsoup"                          % "1.13.1",
+    libraryDependencies += "software.amazon.awssdk"           % "textract"                       % "2.15.19",
+    libraryDependencies += "software.amazon.awssdk"           % "s3"                             % "2.15.19",
+    libraryDependencies += "org.slf4j"                        % "slf4j-api"                      % "1.7.30",
+    libraryDependencies += "org.slf4j"                        % "slf4j-simple"                   % "1.7.30" % Runtime,
   )
 
 addCommandAlias("check", ";+test;scalafmtCheckAll")
--- core/src/main/scala/au/id/tmm/citizenshipregisterscraper/scraping/aws/TextractClient.scala
+++ core/src/main/scala/au/id/tmm/citizenshipregisterscraper/scraping/aws/TextractClient.scala
@@ -4,75 +4,51 @@ import java.io.InputStream
 import java.net.URI
 import java.nio.ByteBuffer
 
-import au.id.tmm.citizenshipregisterscraper.scraping.aws.TextractClient.logger
-import cats.Eval
 import cats.effect.{Bracket, IO}
-import com.amazonaws.handlers.AsyncHandler
-import com.amazonaws.services.textract.model._
-import com.amazonaws.services.textract.{AmazonTextractAsync, AmazonTextractAsyncClientBuilder}
 import org.slf4j.LoggerFactory
+import software.amazon.awssdk.core.client.config.{ClientAsyncConfiguration, SdkAdvancedAsyncClientOption}
+import software.amazon.awssdk.http.SdkHttpClient
+import software.amazon.awssdk.http.apache.ApacheHttpClient
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.textract.TextractAsyncClient
+import software.amazon.awssdk.services.textract.model.S3Object
 
-import scala.concurrent.ExecutionContextExecutorService
+import scala.concurrent.ExecutionContextExecutor
 
-class TextractClient(executionContext: ExecutionContextExecutorService) {
+class TextractClient(
+  executionContext: ExecutionContextExecutor,
+  workingS3: TextractClient.WorkingS3,
+) {
 
-  private val client: Eval[AmazonTextractAsync] = Eval.later(
-    AmazonTextractAsyncClientBuilder
-      .standard()
-      .withExecutorFactory(() => executionContext)
-      .build(),
-  )
-
-  def run(documentLocation: URI): IO[AnalyzeDocumentResult] =
-    for {
-      _        <- IO(logger.info(s"Analyse request for $documentLocation"))
-      document <- makeDocument(documentLocation)
-      request = new AnalyzeDocumentRequest()
-        .withDocument(document)
-        .withFeatureTypes(FeatureType.TABLES, FeatureType.FORMS)
-
-      result <- runAnalyseRequest(request)
-    } yield result
-
-  private def makeDocument(documentLocation: URI): IO[Document] =
-    documentLocation.getScheme match {
-      case "s3" => {
-        val s3Object = new S3Object().withBucket(documentLocation.getHost).withName(documentLocation.getPath)
-        IO.pure(new Document().withS3Object(s3Object))
-      }
-      case _ => streamToBytes(documentLocation).map(bytes => new Document().withBytes(bytes))
-    }
-
-  private def streamToBytes(documentLocation: URI): IO[ByteBuffer] =
-    Bracket[IO, Throwable].bracket(
-      acquire = IO(documentLocation.toURL.openStream()),
-    )(
-      use = readIntoBytes,
-    )(
-      release = stream => IO(stream.close()),
+  private val textractClient = TextractAsyncClient
+    .builder()
+    .asyncConfiguration(
+      ClientAsyncConfiguration
+        .builder()
+        .advancedOption(SdkAdvancedAsyncClientOption.FUTURE_COMPLETION_EXECUTOR, executionContext)
+        .build(),
     )
 
-  private def readIntoBytes(inputStream: InputStream): IO[ByteBuffer] =
-    IO {
-      ByteBuffer.wrap(inputStream.readAllBytes())
-    }
+  def run(documentLocation: URI): IO[Unit] =
+    for {
+      _ <- ???
+    } yield ???
 
-  private def runAnalyseRequest(request: AnalyzeDocumentRequest): IO[AnalyzeDocumentResult] =
-    IO.async[AnalyzeDocumentResult] { callback: (Either[Throwable, AnalyzeDocumentResult] => Unit) =>
-      client.value.analyzeDocumentAsync(request, makeAsyncHandlerUsing(callback))
-    }
+  private def s3ObjectFor(documentLocation: URI): IO[S3Object] =
+    for {
+      _ <- ???
+    } yield ???
 
-  private def makeAsyncHandlerUsing(
-    callback: Either[Throwable, AnalyzeDocumentResult] => Unit,
-  ): AsyncHandler[AnalyzeDocumentRequest, AnalyzeDocumentResult] =
-    new AsyncHandler[AnalyzeDocumentRequest, AnalyzeDocumentResult] {
-      override def onError(exception: Exception): Unit = callback(Left(exception))
-      override def onSuccess(request: AnalyzeDocumentRequest, result: AnalyzeDocumentResult): Unit =
-        callback(Right(result))
-    }
+  private def targetS3URIFor(documentLocation: URI): URI = ???
+
 
 }
 
 object TextractClient {
   private val logger = LoggerFactory.getLogger(getClass)
+
+  final case class WorkingS3(
+    bucket: String,
+    prefix: String = "/",
+  )
 }
