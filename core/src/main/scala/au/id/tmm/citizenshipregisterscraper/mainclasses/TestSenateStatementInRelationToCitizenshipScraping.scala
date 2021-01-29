e495bd55c2dbce5519c68427ee216352d8fcc427
Timothy McCarthy <ebh042@gmail.com>
2021-01-30 08:35:02 +1100

    Run for askew too

--- core/src/main/scala/au/id/tmm/citizenshipregisterscraper/mainclasses/TestSenateStatementInRelationToCitizenshipScraping.scala
+++ core/src/main/scala/au/id/tmm/citizenshipregisterscraper/mainclasses/TestSenateStatementInRelationToCitizenshipScraping.scala
@@ -1,13 +1,23 @@
 package au.id.tmm.citizenshipregisterscraper.mainclasses
 
+import java.net.URI
+
 import au.id.tmm.citizenshipregisterscraper.documents.senate
+import au.id.tmm.citizenshipregisterscraper.documents.senate.{DocumentReference, ListDocuments}
 import au.id.tmm.citizenshipregisterscraper.scraping.SenateStatementInRelationToCitizenship
 import au.id.tmm.citizenshipregisterscraper.scraping.aws.textract.FriendlyClient
 import cats.effect.IO
+import org.slf4j.{Logger, LoggerFactory}
 import sttp.client3.SttpBackend
 
 object TestSenateStatementInRelationToCitizenshipScraping extends MuckingWithDynamoMain {
-  protected val abetzDisclosure: IO[senate.DocumentReference] = disclosureForName("Askew")
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
+
+  private def useDisclosure(documentReference: senate.DocumentReference): Boolean =
+    Set(
+      "Abetz",
+      "Askew",
+    ).exists(documentReference.senatorName.contains)
 
   override def runWithDynamo(
     httpClient: SttpBackend[IO, Any],
@@ -15,13 +25,32 @@ object TestSenateStatementInRelationToCitizenshipScraping extends MuckingWithDyn
     friendlyClient: FriendlyClient,
   ): IO[Unit] =
     for {
-      reference <- abetzDisclosure
+      documentsListUri <- IO(
+        new URI(
+          "https://www.aph.gov.au/Parliamentary_Business/Committees/Senate/Senators_Interests/RegisterQual46thparl",
+        ),
+      )
 
-      analysisResult <- friendlyClient.runAnalysisFor(FriendlyClient.Document.Remote(reference.documentLocation))
+      documents <- ListDocuments.from(documentsListUri)
 
-      statement <- IO.fromEither(SenateStatementInRelationToCitizenship.fromTextract(analysisResult))
-
-      _ <- IO(println(statement))
-
+      _ <-
+        fs2.Stream
+          .chunk[IO, DocumentReference](fs2.Chunk.arraySeq(documents))
+          .filter(useDisclosure)
+          .parEvalMap(maxConcurrent = 4) { documentReference =>
+            friendlyClient
+              .runAnalysisFor(FriendlyClient.Document.Remote(documentReference.documentLocation))
+              .flatMap(analysisResult =>
+                IO.fromEither(SenateStatementInRelationToCitizenship.fromTextract(analysisResult)),
+              )
+              .attempt
+              .flatMap {
+                case Right(parsedStatement) =>
+                  IO(logger.info(s"✅ ${documentReference.senatorName} => $parsedStatement"))
+                case Left(error) => IO(logger.error(s"❌ Failed for ${documentReference.senatorName}", error))
+              }
+          }
+          .compile
+          .drain
     } yield ()
 }
